d4f3b18a42b6f436a4e24e840b52394349765251
Juan David Lopez Londono <juandavid@jdlondono.com>
2020-05-31 23:38:22 -0700

    Get the app container to span the full height of the viewport.

--- src/components/App/AppStyledComponents.js
+++ src/components/App/AppStyledComponents.js
@@ -7,16 +7,40 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+let AppContainerStyled = styled(Container)`
+   /* Use ampserands for this to acquire precedence over the bootstrap components */
+   &&& {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+   }
+`;
+
+let AppContainerRow = styled(Row)`
+   /* Use ampserands for this to acquire precedence over the bootstrap components */
+   &&& {
+    flex: 1;
+   }
+`;
+
+let AppContainerCol = styled(Col)`
+    /* Use ampserands for this to acquire precedence over the bootstrap components */
+    &&& {    
+        display: flex;
+        flex-direction: column;
+    }
+`;
+
 const AppContainer = (props) => {
     return (
-        <Container fluid={ props.fluid } className="app-container">
-            <Row className="app-container-row">
-                <Col className="padding-side-0 no-gutters app-container-col">
+        <AppContainerStyled fluid={ props.fluid } className="app-container">
+            <AppContainerRow className="app-container-row">
+                <AppContainerCol className="padding-side-0 no-gutters app-container-col">
                     { props.children }
-                </Col>
-            </Row>
+                </AppContainerCol>
+            </AppContainerRow>
            
-        </Container>
+        </AppContainerStyled>
     ); //end return statement
 }; //end AppContainer
  
