3567b02978daf28ea5f53b8d8b41771b24ed8516
Juan David Londoño <juandavid@jdlondono.com>
2020-06-13 10:41:43 -0700

    Changes from everywhere.

---
+++ src/components/Header/NavigationBar/NavigationBarStyledComponents.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+//Import React Bootstrap components
+import Navbar from 'react-bootstrap/Navbar';
+
+/* 
+    .bg-iris-yellow {
+        background-color: #FFF308;
+    }
+
+    .bg-iris-purple {
+        background-color: #5425BA;
+    }
+*/
+
+
+let NavBar = styled(Navbar)`
+  background: #5425BA;
+`; //end NavBar styled-component
+
+export { NavBar };
+
+
