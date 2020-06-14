71e7a948c9a96c04abb86bd6bd2d8982581d4570
Juan David Londoño <juandavid@jdlondono.com>
2020-06-13 23:21:46 -0700

    Create the top menu bar

--- src/components/Header/NavigationBar/NavBarToggle/NavBarToggle.js
+++ src/components/Header/NavigationBar/NavBarToggle/NavBarToggle.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+//Import styled components
+import { MenuBarLine, TopMenuBarLine } from './NavBarToggleStyledComponents.js'
 class NavBarToggle extends Component {
     constructor(props) {
         super(props);
@@ -27,9 +29,9 @@ class NavBarToggle extends Component {
     toggleClass2 = () => {
         const currentState = this.state.collapsed;
         this.setState({ collapsed: !currentState});
-        let NavBarCollapse = document.getElementById("basic-navbar-nav");
 
-        NavBarCollapse.classList.toggle("show");
+        // let NavBarCollapse = document.getElementById("basic-navbar-nav");
+        //     NavBarCollapse.classList.toggle("show");
     }; //end toggleClass2
 
     /*
@@ -39,15 +41,19 @@ class NavBarToggle extends Component {
     }); */
     render = () => {
         return ( 
-            <button aria-controls="basic-navbar-nav" 
-                    type="button" 
-                    aria-label="Toggle navigation" 
-                    className={ "navbar-toggler "  + ( this.state.collapsed ? "collapsed" : "") } 
-                    id="navbarToggle"
-                    onClick={this.toggleClass2}>
-                <span className="icon-bar top-bar"></span>
-                <span className="icon-bar middle-bar"></span>
-                <span className="icon-bar bottom-bar"></span>				
+            <button 
+                    aria-controls   =   "basic-navbar-nav" 
+                    type            =   "button" 
+                    aria-label      =   "Toggle navigation" 
+                    className       =   { "navbar-toggler "  
+                                            + ( this.state.collapsed ? "collapsed" : "") 
+                                        } 
+                    id              =   "navbarToggle"
+                    onClick         =   { this.toggleClass2 }
+            >
+                <TopMenuBarLine className="menubar-line top-bar" collapsed={this.state.collapsed}></TopMenuBarLine>
+                <MenuBarLine className="menubar-line middle-bar"></MenuBarLine>
+                <MenuBarLine className="menubar-line bottom-bar"></MenuBarLine>				
             </button>
         );
     }; //end render()
