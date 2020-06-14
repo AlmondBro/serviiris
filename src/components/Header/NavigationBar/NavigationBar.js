0661c59078a9a6266d98b55cdf42450848d2e3c4
Juan David Londoño <juandavid@jdlondono.com>
2020-06-13 21:54:51 -0700

    Turn sample NavBar code into styled components

--- src/components/Header/NavigationBar/NavigationBar.js
+++ src/components/Header/NavigationBar/NavigationBar.js
@@ -11,41 +11,12 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import NavBarToggle from './NavBarToggle.js'
 
 //Import styled components
-import { NavBar, NavStyled, NavBarCollapse, NavLink as Link } from './NavigationBarStyledComponents.js';
+import  {   NavBar, NavContainer, NavLogoContainer, NavLogoContainerLink,
+            Nav, NavUl, NavLi, NavLink
+        } from './NavigationBarStyledComponents.js';
 
-const NavigationBar = (props) => {
-    return (
-            <NavBar 
-                    bg          =   "green" 
-                    variant     =   "dark" 
-                    expand      =   "sm" 
-                    className   =   "navigation-bar"
-            >
-                <Navbar.Brand 
-                    className   =   "collapsedNav-logo"
-                    >
-                    <Link 
-                        to      =   "/home"
-                        title   =   "Home"
-                    >
-                        {/* <img src="/assets/img/harza-tapes-logo.svg" 
-                             alt="Harza Tapes" 
-                             title="Home"
-                             id="harza-logo-mobile"
-                        /> */}
-                    </Link>
-                </Navbar.Brand>
-                {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-                {/*  TODO: Work on the logic for the NavBarToggle */}
-                <NavBarToggle   
-                    aria-controls   =   "basic-navbar-nav" 
-                />
-                <NavBarCollapse 
-                    className   =   "NavBarCollapse" 
-                    id          =   "basic-navbar-nav"
-                >
-                    <NavStyled>
-                        <Link 
+/*
+    <Link 
                             to              =   "/home" 
                             activeClassName =   "navLink-current"  
                             className       =   "navLinks nav-link"
@@ -62,15 +33,78 @@ const NavigationBar = (props) => {
                         <Link to="/products" activeClassName="navLink-current"  className="navLinks nav-link">Servicios</Link> 
                         <Link to="/location" activeClassName="navLink-current"  className="navLinks nav-link">Contáctanos</Link> 
                         <Link to="/contact-us" activeClassName="navLink-current" className="navLinks nav-link">Trabaja con Nosotros</Link> 
-                    </NavStyled>
-                    {/* <Form inline>
-                        <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="outline-light">
-                            <FontAwesomeIcon icon={faSearch} />
-                        </Button>
-                    </Form> */}
-                </NavBarCollapse>
-            </NavBar>     
+*/
+const NavigationBar = (props) => {
+    return (
+            <NavBar className="navigation-bar">
+                <NavContainer className="nav-container">
+                    {/* <NavLogoContainer className="nav-logo-container">
+                        <NavLogoContainerLink href="#!">Logo</NavLogoContainerLink>
+                    </NavLogoContainer> */}
+                    <Nav>
+                        {/* <div class="nav-mobile">
+                            <a id="nav-toggle" href="#!">
+                                <span></span>
+                            </a>
+                        </div> */}
+                        
+                        <NavUl className="nav-ul">
+                            <NavLi className="nav-li">
+                                <NavLink 
+                                    href="#!" 
+                                    className="nav-li"
+                                >
+                                Home
+                            </NavLink>
+                            </NavLi>
+                            <NavLi>
+                                <NavLink href="#!">About</NavLink>
+                            </NavLi>
+                            {/* <li>
+                                <a href="#!">Services</a>
+                                <ul class="nav-dropdown">
+                                    <li>
+                                        <a href="#!">Web Design</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">Web Development</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">Graphic Design</a>
+                                    </li>
+                                </ul>
+                            </li> */}
+                            <NavLi>
+                                <NavLink href="#!">Pricing</NavLink>
+                            </NavLi>
+                            {/* <li>
+                                <a href="#!">Portfolio</a>
+                                <ul class="nav-dropdown">
+                                    <li>
+                                        <a href="#!">Web Design</a>
+                                        </li>
+                                        <li>
+                                        <a href="#!">Web Development</a>
+                                        </li>
+                                        <li>
+                                        <a href="#!">Graphic Design</a>
+                                    </li>
+                                </ul>
+                            </li> */}
+                            <NavLi>
+                                <NavLink href="#!">Contact</NavLink>
+                            </NavLi>
+                        </NavUl>
+                </Nav>
+                {/* <form>
+                    <p style={{margin: 0}}>
+                        <label>Hi</label>
+                        <input type="text" id="search"/>
+                            
+                    </p>
+                </form> */}
+            </NavContainer>
+        </NavBar>
     ); //end return
 }; //end Navigation()
 
