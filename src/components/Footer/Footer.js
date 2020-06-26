4700f343a13b49f42fb674e068cc28aae44ecc1e
Juan David Londoño <juandavid@jdlondono.com>
2020-06-26 11:35:04 -0700

    Construct HTML for footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,8 +8,37 @@ import Col from 'react-bootstrap/Col';
 
 const Footer = (props) => {
     return (
-        <FooterStyled className="footer-container">
-            Hi
+        <FooterStyled className="row footer-container">
+            <div className="col" id="footer-col-1">
+                <div className="row" id="footer-col1-row">
+                    <div className="col" id="footer-col1-row-col1">
+
+                    </div>
+
+                    <address className="col" id="footer-address"> 
+                        <a  href =   "#"
+                            id  =   "footer-direction"
+                        >
+                            Carerra 24c #4-54 Cali, Valle del Cauca, Colombia
+                        </a>
+                        <a  href =   "mailto:iristeamco@gmail.com"
+                            id  =   "footer-email"
+                        >
+                            iristeamco@gmail.com
+                        </a>
+                        <a  href    =   "https://wa.link/aloo0e"
+                            id      =   "footer-phone">
+
+                        </a>
+                    </address>
+                </div>
+            </div>
+            <div className="col" id="footer-col-2">
+                Col 2
+            </div>
+            <div className="col" id="footer-col-3">
+                Col 3
+            </div>
         </FooterStyled>
     ); //end return statement
 
