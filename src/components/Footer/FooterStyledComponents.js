229ab4f58547aab72afad0d29870f355b28c7d87
Juan David Londoño <juandavid@jdlondono.com>
2020-07-01 21:59:16 -0700

    Fix the footer by using bottom: 0 instead of a top value on mobile

--- src/components/Footer/FooterStyledComponents.js
+++ src/components/Footer/FooterStyledComponents.js
@@ -97,7 +97,7 @@ const FooterColumn = styled('div')`
 
         border-top: ${props => props.socialMediaColumn ? "0px solid gray" : "0px solid transparent"};
 
-        top: ${props => props.socialMediaColumn ? "92%" : "0%"};
+        bottom: ${props => props.socialMediaColumn ? "0%" : "0%"};
         background-color:  ${props => props.socialMediaColumn ? "#2F2E2E" : "none"};
 
         z-index:  ${props => props.socialMediaColumn ? "1" : "0"};
