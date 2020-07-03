ba7f0ef7c8e7f81fdbb8ba705e0debed46903587
Juan David Londoño <juandavid@jdlondono.com>
2020-07-02 20:12:22 -0700

    Start to shape the page.

--- src/components/Pages/AboutUs/AboutUs.js
+++ src/components/Pages/AboutUs/AboutUs.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-import { AboutUsPageContainer } from './AboutUs_StyledComponents.js';
+import { AboutUsPageContainer, OurHistorySection, IrisWorkerFemaleSVG, OurHistoryArticle, WhoWeAreSection  } from './AboutUs_StyledComponents.js';
 
 const AboutUs = (props) => {
     return (
         <AboutUsPageContainer id="about-us-page-container">
-            <div className="row">Row1</div>
-            <div className="row">Row2</div>
+            <OurHistorySection className="row" id="our-history-section">
+                <OurHistoryArticle>
+                    <h3>Nuestra Historia...</h3>
+                    <p>
+                        Un miércoles, 18 de Marzo del 2020 nace como un emprendimiento y, a su 
+                        vez, un anhelo de dos jóvenes universitarios llenos de amor y energía 
+                        que durante el confinamiento obligatorio se lanzan a hacer realidad un 
+                        proyecto de mensajería.
+                    </p>
+                    <p>
+                        En la búsqueda de un nombre para nuestra empresa nos encontramos en la 
+                        mitología griega a Iris quien fue escogida como la Diosa de la mensajería
+                         por su capacidad de desplazarse a la velocidad del viento. Era una de 
+                         las encargadas de entregarles los mensajes de los dioses a los humanos 
+                         y viceversa. De allí nuestro nombre.
+                    </p>
+                    <IrisWorkerFemaleSVG src="./assets/img/iris-worker-female.svg"/>
+                </OurHistoryArticle>
+            </OurHistorySection>
+            <WhoWeAreSection className="row" id="who-we-are-section">Row2</WhoWeAreSection>
         </AboutUsPageContainer>
     );  //end return statement
 }; //end Conocenos() component
