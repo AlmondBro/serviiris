e6dbfbca3fed90ff3443bd23d0bad730779059e9
Juan David Londoño <juandavid@jdlondono.com>
2020-07-05 15:26:35 -0700

    Create JSON object with the services' details

--- src/components/Pages/Specialties/Specialties.js
+++ src/components/Pages/Specialties/Specialties.js
@@ -2,16 +2,37 @@ import React from 'react';
 
 import { SpecialtiesPageContainer, SpecialitesSection, SpecialitesSectionRow, SpecialtiesArticleColumn, } from './Specialties_StyledComponents.js';
 
+import serviceColumnsJSON from './services-data.json';
+
 const Specialties = (props) => {
+    const generateSpecialtiesColumns  = (jsonFile) => {
+        const serviceColumnsObjectArray = JSON.parse(serviceColumnsJSON);
+        serviceColumnsObjectArray["serviceColumns"].forEach( (serviceColumn, index) => {
+            return  (   
+                        <SpecialtiesArticleColumn
+                            id          =   { serviceColumn.id }
+                            imageAlt    =   { serviceColumn.imageAlt }
+                            image       =   { serviceColumn.image }
+                            buttonTitle =   { serviceColumn.buttonTitle }
+                            title       =   { serviceColumn.title }
+                            description =   { serviceColumn.description }
+                            Key         =   { index }
+                        />
+                    ); //end inner return statement
+        }); //end forEach()
+    }; //end generateSpecialtiesColumns()
+
     return (
         <SpecialtiesPageContainer id="row no-gutters specialties-page-container">
             <SpecialitesSection className="col-12 specialties-section" id="specialties-section">
                 <SpecialitesSectionRow className="row no-gutters">
+                    {/* <SpecialtiesArticleColumn/>
                     <SpecialtiesArticleColumn/>
                     <SpecialtiesArticleColumn/>
-                    <SpecialtiesArticleColumn/>
-                    <SpecialtiesArticleColumn/>
-                    
+                    <SpecialtiesArticleColumn/> */}
+                    {
+                        generateSpecialtiesColumns(serviceColumnsJSON)
+                    }
                 </SpecialitesSectionRow>
             </SpecialitesSection>
             
