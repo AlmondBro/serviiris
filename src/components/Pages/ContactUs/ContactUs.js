565913e20414442288e0f1de6bd35757c0df20d6
Juan David Londoño <juandavid@jdlondono.com>
2020-07-06 22:33:48 -0700

    Create basic squares

--- src/components/Pages/ContactUs/ContactUs.js
+++ src/components/Pages/ContactUs/ContactUs.js
@@ -11,19 +11,50 @@ const ContactUs = (props) => {
     return (
         <ContactUsPageContainer id="row no-gutters ContactUs-page-container">
             <ContactUsSection className="col-12 p-0 ContactUs-section" id="ContactUs-section">
-                    <ContactUsSectionHeader>
-                        <h3>Contáctanos</h3>
-                        <h4>Queremos saber de ti</h4>
-                    </ContactUsSectionHeader>
+                <ContactUsSectionHeader>
+                    <h3>Contáctanos</h3>
+                    <h4>Queremos saber de ti</h4>
+                </ContactUsSectionHeader>
+
                 <ContactUsSectionRow className="row no-gutters">
-                    <ContactUsSectionCol />
-                    <ContactUsSectionCol />
-                    <ContactUsSectionCol />
-                    <ContactUsSectionCol />
+                    {/* bgColor, title, faIcon, description, link,  */}
+                    <ContactUsSectionCol 
+                        bgColor     =   "white"
+                        title       =   { "Centro de soluciones" }
+                        faIcon      =   {  faPaperPlane }
+                        description =   { "iristeamco@gmail.com"}
+                        link        =   { "mailto:iristeamco@gmail.com"}
+                    />
+                      <ContactUsSectionCol 
+                        bgColor     =   "red"
+                        title       =   { "Comunícate al" }
+                        faIcon      =   {  faPaperPlane }
+                        description =   { "+57 (311) 718-4447"}
+                        link        =   { "tel:+57 (315) 453-9422"}
+                    />
+                       <ContactUsSectionCol 
+                        bgColor     =   "pink"
+                        title       =   { "Siguenos en Instagram" }
+                        faIcon      =   {  faPaperPlane }
+                        description =   { "@iristeam.co"}
+                        link        =   { "https://www.instagram.com/iristeam.co/?hl=es-la"}
+                    />
+                       <ContactUsSectionCol 
+                        bgColor     =   "blue"
+                        title       =   { "Encuentranos en Facebook" }
+                        faIcon      =   {  faPaperPlane }
+                        description =   { "Iris-Team"}
+                        link        =   { "https://www.facebook.com/Iris-Team-103434521342003/"}
+                    />
+                       <ContactUsSectionCol 
+                        bgColor     =   "green"
+                        title       =   { "Escribenos en Whatsapp" }
+                        faIcon      =   {  faPaperPlane }
+                        description =   { "iristeamco@gmail.com"}
+                        link        =   { "mailto:iristeamco@gmail.com"}
+                    />
                 </ContactUsSectionRow>
-                
             </ContactUsSection>
-            
         </ContactUsPageContainer>
     );  //end return statement
 }; //end Conocenos() component
